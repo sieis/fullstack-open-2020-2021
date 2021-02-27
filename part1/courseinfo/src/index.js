9cc8e6ef01e3d0005cf5a2d759df4deb79a0ede1
Eamonn Cottrell <eamonncottrell@gmail.com>
2021-02-27 14:04:16 -0500

    end of courseinfo 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
+const Content=(props)=>{
+  return(
+    <>
+      <Part part={props.part1} exercise={props.exercises1}/>
+      <Part part={props.part2} exercise={props.exercises2}/>
+      <Part part={props.part3} exercise={props.exercises3}/>
+    </>
+  )
+}
 
 const Header = (props)=>{
   return(
@@ -8,13 +17,9 @@ const Header = (props)=>{
   )
 }
 
-const Content=(props)=>{
+const Part = (props)=>{
   return(
-    <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
-    </>
+    <p>{props.part}: {props.exercise}</p>
   )
 }
 
