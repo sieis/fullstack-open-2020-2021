504268de2ab246f154588bf53717b100ff8ec852
Eamonn Cottrell <eamonncottrell@gmail.com>
2021-03-11 14:20:06 -0500

    unicafe 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Display = (props) => <p>{props.text}: {props.value}</p>
+
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
+
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const positive = good / all
 
   const setGoodValue = (newValue) => () => {
     setGood(newValue)
@@ -28,9 +33,11 @@ const App = () => {
       <Button handleClick={setNeutralValue(neutral + 1)} text='neutral' />
       <Button handleClick={setBadValue(bad + 1)} text='bad' />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Display text='good' value={good} />
+      <Display text='neutral' value={neutral} />
+      <Display text='bad' value={bad} />
+      <Display text='all' value={all} />
+      <Display text='positive' value={positive} />
     </>
   )
 }
