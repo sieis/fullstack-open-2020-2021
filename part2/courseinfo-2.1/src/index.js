05c658ab12d203c32300433e4487f3c0dc6d37ae
Eamonn Cottrell <eamonncottrell@gmail.com>
2021-03-25 15:43:55 -0400

    working after 2.1

---
+++ part2/courseinfo-2.1/src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Part = ({ name, exercise }) => {
+  return (
+    <p>{name} {exercise}</p>
+  )
+}
+
+{/* <p>
+  {course.parts[0].name}
+</p> */}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+    </>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part key={part.id} name={part.name} exercise={part.exercises} />
+      )}
+    </div>
+  )
+}
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Added Part!??',
+        exercises: 15,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
